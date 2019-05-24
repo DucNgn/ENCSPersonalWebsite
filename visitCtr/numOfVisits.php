37e42c8c49c5f50e69450e20d730b2766d0b5c03
duke <triduc2311m@gmail.com>
2019-05-24 01:59:00 -0400

    fixed no of visits

--- visitCtr/numOfVisits.php
+++ visitCtr/numOfVisits.php
@@ -9,12 +9,11 @@ setcookie("timeVisited", time(), time() + 3600*24*365);
 
 if (isset($_COOKIE["numOfVisits"])) {
 	  $last    = date("D F d G:i:s e Y ", $_COOKIE['timeVisited']);
-      $message = "This is the $count times that you are visiting my webpage <br /> Welcome back to my website! Your last visited was recorded on ".$last;
+      echo "This is the $count times that you are visiting my webpage <br /> Welcome back to my website! Your last visited was recorded on ".$last;
 }
 else {
-      $message = "<strong> Welcome to my webpage! It is your first time that you are here </strong>";
+      echo "<strong> Welcome to my webpage! It is your first time that you are here </strong>";
 
 }
 
-echo $message;
 ?>
