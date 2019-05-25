796ad12b013be69a3f848abf46fdc97fe66ccf84
duke <triduc2311m@gmail.com>
2019-05-24 20:34:49 -0400

    add project page, setup project page with GitHub Infor, add Floating button, parallax effect for project page

---
+++ project.php
@@ -0,0 +1,47 @@
+<?php include './share/header.html'; 
+	  include './share/body.html';
+?>
+
+<!-- PROJECT PAGE 'S STYLE -->
+<link id="pagestyle" rel="stylesheet" type="text/css" href="./projectStyle/style.css">
+
+<!-- SCRIPT FOR TITLE CHANGING -->
+<script>
+changeTitle("Duke's Projects");
+</script>
+
+<!-- Floating button -->
+	<a href="../index.php" class="float">
+        <i class="my-float"></i>
+    </a>
+    <div class="label-container">
+    	<div class="label-text"> Home </div>
+    	<i class="fa fa-play label-arrow"></i>
+    </div>
+	<!-- END OF FLOATING BUTTON -->
+
+<h1 id="ctr">PROJECTS</h1>
+
+<!--PARALLAX EFFECT -->
+	<div class="GitHubparallax"></div>
+    <div>
+<br />
+<label id ="ctr"><h3>GitHub</h3> </label>
+<ul>
+	<li>Access my <a href="https://github.com/DukeNgn" target="_blank">GitHub</a></li>
+	<blockquote>GitHub is where I keep almost every project I have done.</blockquote>
+    <li><a href="https://dukengn.github.io/" target="_blank">GitHub Page</a> </li>
+	<blockquote>GitHub Page is where I keep track of courses I have completed at school.</blockquote>
+	<li>Learn more about <a href ="https://www.youtube.com/watch?v=w3jLJU7DT5E" target="_blank">GitHub [video] </a></li>
+	<blockquote>"...GitHub is an American web-based hosting service for version control using Git. It is mostly used for computer code. It offers all of the distributed version control and source code management (SCM) functionality of Git as well as adding its own features. It provides access control and several collaboration features such as bug tracking, feature requests, task management, and wikis for every project... -<a href="https://en.wikipedia.org/wiki/GitHub" target="_blank">Wikipedia</a>-"</blockquote>
+</ul>
+<br />
+</div>
+	<div class="GitHubparallax"></div>
+	<!--END OF PARALLAX EFFECT -->
+<br />
+
+
+
+
+<?php include './share/footer.html' ?>
