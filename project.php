31c2ccabbce746c775ceea90812ee856b0eb2270
duke <triduc2311m@gmail.com>
2019-05-25 21:14:16 -0400

    add showcase project for comp 249, change minor detail

--- project.php
+++ project.php
@@ -33,7 +33,7 @@ changeTitle("Duke's Projects");
 <label id ="ctr"><h3>GitHub</h3> </label>
 <ul>
 	<li>Access my <a href="https://github.com/DukeNgn" target="_blank">GitHub</a></li>
-	<blockquote>GitHub is where I keep almost every project I have done.</blockquote>
+	<blockquote>GitHub is where I keep every project I have done.</blockquote>
     <li><a href="https://dukengn.github.io/" target="_blank">GitHub Page</a> </li>
 	<blockquote>GitHub Page is where I keep track of courses I have completed at school.</blockquote>
 	<li>Learn more about <a href ="https://www.youtube.com/watch?v=w3jLJU7DT5E" target="_blank">GitHub [video] </a></li>
@@ -47,24 +47,89 @@ changeTitle("Duke's Projects");
 <br />
 
 <!-- PROJECT 1 SHOWCASE: COMP 248 -->
-<label id ="ctr"> NO.1: COMP 248: OBJECT-ORIENTED PROGRAMMING I</label>
-<label id ="ctr"><a href="https://github.com/DukeNgn/COMP248-OOP1" target="_blank">PROJECT REPO</a></label>
+<label id ="ctr"><h2>COMP 248: OBJECT-ORIENTED PROGRAMMING I</h2></label>
+<label id ="ctr"><a href="https://github.com/DukeNgn/COMP248-OOP1" target="_blank">REPOSITORY</a></label>
+<br />
 
+<h3><strong>FINAL PROJECT: QWIXX GAME</strong></h3>
+<h4 id="ctr"><a href="https://github.com/DukeNgn/COMP248-OOP1/tree/master/Assignment%234" target="_blank">[Source Code]</a></h4>
 <div class="row">
-<div class="column">
+  <div class="column">
 	<img src="./projectStyle/image/COMP248/Instruc.png" alt="COMP248Instruction" style="width:100%">
 	</div>
-<div class="column">
+  <div class="column">
 	<img src="./projectStyle/image/COMP248/COMP248.gif" alt="COMP248ScreenShot.gif" style="width:100%">
 	</div>
 </div>
 
-<h4 id="ctr"> Demo of final project: Qwixx game</h4>
+<h4 id="ctr"> Demo Screenshots</h4>
+
+
 <!--END OF PROJECT 1 SHOWCASE: COMP 248 -->
 
 <br />
 <br />
+
+<!-- PROJECT 2 SHOWCASE: COMP 249 -->
+
+<label id ="ctr"><h2>COMP 249: OBJECT-ORIENTED PROGRAMMING II</h2></label>
+<label id="ctr"><a href="https://github.com/DukeNgn/COMP249-OOP2-with-java">REPOSITORY</a></label>
+
+<!--showcase 1: extract Dictionary -->
+<h3><strong>NO.1: DICTIONARY EXTRACT TOOL</strong></h3>
+<h4 id ="ctr"><a href="https://github.com/DukeNgn/COMP249-OOP2-with-java/tree/master/ArrayList%20%26%20LinkedArray/subDictionaryCreator" target="_blank">[Source Code]</a></h4>
+<div class="row">
+   <div class="column">
+	 <img src="./projectStyle/image/COMP249/before.png" alt="beforeDictionary" style="width:100%">
+	 </div>
+  <div class="column">
+	<img src="./projectStyle/image/COMP249/result.gif" alt="ResultDictionary" style="width:100%">
+	 </div>
+</div>
+<br />
+<h4 id="ctr"> Dictionary extract tool: Extract, and sort words alphabetically from a source text file</h4>
+
+<br />
+<!-- END OF EXTRACT DICTIONARY -->
+
+<!-- SHOWCASE 2: FIND, GENERATE REFERENCES -->
+<br />
+<h3><strong>NO.2: REFERENCES & CITATION GENERATOR</strong></h3>
+<h4 id ="ctr"><a href="https://github.com/DukeNgn/COMP249-OOP2-with-java/tree/master/authorBibCreator" target="_blank">[Source Code]</a></h4>
+
+<div class="row">
+   <div class="column">
+	 <img src="./projectStyle/image/COMP249/beforeLatex.png" alt="" style="width:100%">
+	 </div>
+  <div class="column">
+	<img src="./projectStyle/image/COMP249/ProcessingLatex.gif" alt="" style="width:100%">
+	 </div>
+</div>
+<br />
+<h5 id ="ctr"> EXAMPLE OF INPUT FILE & SCREENSHOT DURING PROCESSING </h5>
+
+<div class="row">
+   <div class="column">
+	 <img src="./projectStyle/image/COMP249/ACM.png" alt="" style="width:100%">
+	 </div>
+  <div class="column">
+	<img src="./projectStyle/image/COMP249/IEEE.png" alt="" style="width:100%">
+	 </div>
+</div>
+<h5 id ="ctr"> DEMO EXAMPLE OF RESULT IN 2 CITATION FORMATS </h5>
+<ul>
+	<li id ="ctr"> <a href="https://www.acm.org/publications/authors/reference-formatting" target="_blank">ACM (Association for Computing Machinery)</a> </li>
+	<li id ="ctr"><a href="http://www.ijssst.info/info/IEEE-Citation-StyleGuide.pdf" target="_blank">IEEE</a></li>
+</ul>
+
+<br />
+
+
+<!-- END OF REFERENCE GENERATOR -->
+
+<!-- END OF PROJECT 2 SHOWCASE: COMP249 -->
 <br />
 <br />
+
 
 <?php include './share/footer.html' ?>
