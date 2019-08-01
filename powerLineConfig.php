bf6bf8a4bc32e4028963ec65344dec7c1e649f01
Duke_Ngn <triduc2311m@gmail.com>
2019-07-31 20:34:46 -0400

    Added powerline style installation guide, embed gist

---
+++ powerLineConfig.php
@@ -0,0 +1,23 @@
+<?php include './share/header.html';
+include './share/body.html';
+?>
+
+<!--  PAGE 'S STYLE -->
+<link id="pagestyle" rel="stylesheet" type="text/css" href="./projectStyle/style.css">
+
+<!-- SCRIPT FOR TITLE CHANGING -->
+<script>
+	changeTitle("Powerline-Configure");
+</script>
+
+<p id="holdPoint"></p>
+<br />
+<h1 id="ctr">POWERLINE-STYLE FOR TERMINAL</h1>
+<br />
+
+<!--EMBED GIST PAGE --->
+<span id = 'ctr'>
+<script src="https://gist.github.com/DukeNgn/9312a65e1535b3b1bffd9fed2c892079.js"></script>
+</span>
+
+<?php include './share/footer.html' ?>
--- project.php
+++ project.php
@@ -1,5 +1,5 @@
-<?php include './share/header.html'; 
-	  include './share/body.html';
+<?php include './share/header.html';
+include './share/body.html';
 ?>
 
 <!-- PROJECT PAGE 'S STYLE -->
@@ -7,47 +7,51 @@
 
 <!-- SCRIPT FOR TITLE CHANGING -->
 <script>
-changeTitle("Duke's Projects");
+	changeTitle("Duke's Projects");
 </script>
 
-<p id ="holdPoint"></p>
+<p id="holdPoint"></p>
 <br />
 <h1 id="ctr">PROJECTS</h1>
 <br />
 
 <!--PARALLAX EFFECT -->
-	<div class="GitHubparallax"></div>
-    <div>
-<br />
-<label id ="ctr"><h3>GitHub</h3> </label>
-  <ul>
-	<li>Access my <a href="https://github.com/DukeNgn" target="_blank">GitHub</a></li>
-	<blockquote>GitHub is where I keep every project I have done.</blockquote>
-    <li><a href="https://dukengn.github.io/" target="_blank">GitHub Page</a> </li>
-	<blockquote>GitHub Page is where I keep track of courses I have completed at school.</blockquote>
-	<li>Learn more about <a href ="https://www.youtube.com/watch?v=w3jLJU7DT5E" target="_blank">GitHub [video] </a></li>
-	<blockquote>"...GitHub is an American web-based hosting service for version control using Git. It is mostly used for computer code. It offers all of the distributed version control and source code management (SCM) functionality of Git as well as adding its own features. It provides access control and several collaboration features such as bug tracking, feature requests, task management, and wikis for every project... -<a href="https://en.wikipedia.org/wiki/GitHub" target="_blank">Wikipedia</a>-"</blockquote>
-  </ul>
-<br />
-    </div>
-	    <div class="GitHubparallax"></div>
+<div class="GitHubparallax"></div>
+<div>
+	<br />
+	<label id="ctr">
+		<h3>GitHub</h3>
+	</label>
+	<ul>
+		<li>Access my <a href="https://github.com/DukeNgn" target="_blank">GitHub</a></li>
+		<blockquote>GitHub is where I keep every project I have done.</blockquote>
+		<li><a href="https://dukengn.github.io/" target="_blank">GitHub Page</a> </li>
+		<blockquote>GitHub Page is where I keep track of courses I have completed at school.</blockquote>
+		<li>Learn more about <a href="https://www.youtube.com/watch?v=w3jLJU7DT5E" target="_blank">GitHub [video] </a></li>
+		<blockquote>"...GitHub is an American web-based hosting service for version control using Git. It is mostly used for computer code. It offers all of the distributed version control and source code management (SCM) functionality of Git as well as adding its own features. It provides access control and several collaboration features such as bug tracking, feature requests, task management, and wikis for every project... -<a href="https://en.wikipedia.org/wiki/GitHub" target="_blank">Wikipedia</a>-"</blockquote>
+	</ul>
+	<br />
+</div>
+<div class="GitHubparallax"></div>
 <!--END OF PARALLAX EFFECT -->
 
 <br />
 
 <!-- PROJECT 1 SHOWCASE: COMP 248 -->
-<label id ="ctr"><h2>COMP 248: OBJECT-ORIENTED PROGRAMMING I</h2></label>
-<label id ="ctr"><a href="https://github.com/DukeNgn/COMP248-OOP1" target="_blank">REPOSITORY</a></label>
+<label id="ctr">
+	<h2>COMP 248: OBJECT-ORIENTED PROGRAMMING I</h2>
+</label>
+<label id="ctr"><a href="https://github.com/DukeNgn/COMP248-OOP1" target="_blank">REPOSITORY</a></label>
 <br />
 
 <h3><strong>FINAL PROJECT: QWIXX GAME</strong></h3>
 <h4 id="ctr"><a href="https://github.com/DukeNgn/COMP248-OOP1/tree/master/Assignment%234" target="_blank">[Source Code]</a></h4>
 <div class="row">
-  <div class="column">
-	<img src="./projectStyle/image/COMP248/Instruc.png" alt="COMP248Instruction" style="width:100%">
+	<div class="column">
+		<img src="./projectStyle/image/COMP248/Instruc.png" alt="COMP248Instruction" style="width:100%">
 	</div>
-  <div class="column">
-	<img src="./projectStyle/image/COMP248/COMP248.gif" alt="COMP248ScreenShot.gif" style="width:100%">
+	<div class="column">
+		<img src="./projectStyle/image/COMP248/COMP248.gif" alt="COMP248ScreenShot.gif" style="width:100%">
 	</div>
 </div>
 
@@ -58,19 +62,21 @@ changeTitle("Duke's Projects");
 <br />
 
 <!-- PROJECT 2 SHOWCASE: COMP 249 -->
-<label id ="ctr"><h2>COMP 249: OBJECT-ORIENTED PROGRAMMING II</h2></label>
+<label id="ctr">
+	<h2>COMP 249: OBJECT-ORIENTED PROGRAMMING II</h2>
+</label>
 <label id="ctr"><a href="https://github.com/DukeNgn/COMP249-OOP2-with-java">REPOSITORY</a></label>
 
 <!--showcase 1: extract Dictionary -->
 <h3><strong>NO.1: DICTIONARY EXTRACT TOOL</strong></h3>
-<h4 id ="ctr"><a href="https://github.com/DukeNgn/COMP249-OOP2-with-java/tree/master/ArrayList%20%26%20LinkedArray/subDictionaryCreator" target="_blank">[Source Code]</a></h4>
+<h4 id="ctr"><a href="https://github.com/DukeNgn/COMP249-OOP2-with-java/tree/master/ArrayList%20%26%20LinkedArray/subDictionaryCreator" target="_blank">[Source Code]</a></h4>
 <div class="row">
-   <div class="column">
-	 <img src="./projectStyle/image/COMP249/before.png" alt="beforeDictionary" style="width:100%">
-	 </div>
-  <div class="column">
-	<img src="./projectStyle/image/COMP249/result.gif" alt="ResultDictionary" style="width:100%">
-	 </div>
+	<div class="column">
+		<img src="./projectStyle/image/COMP249/before.png" alt="beforeDictionary" style="width:100%">
+	</div>
+	<div class="column">
+		<img src="./projectStyle/image/COMP249/result.gif" alt="ResultDictionary" style="width:100%">
+	</div>
 </div>
 <br />
 <h4 id="ctr"> Dictionary extract tool: Extract, and sort words alphabetically from a source text file</h4>
@@ -81,53 +87,68 @@ changeTitle("Duke's Projects");
 <!-- SHOWCASE 2: GENERATE REFERENCES -->
 <br />
 <h3><strong>NO.2: REFERENCES & CITATION GENERATOR</strong></h3>
-<h4 id ="ctr"><a href="https://github.com/DukeNgn/COMP249-OOP2-with-java/tree/master/authorBibCreator" target="_blank">[Source Code]</a></h4>
+<h4 id="ctr"><a href="https://github.com/DukeNgn/COMP249-OOP2-with-java/tree/master/authorBibCreator" target="_blank">[Source Code]</a></h4>
 
 <div class="row">
-   <div class="column">
-	 <img src="./projectStyle/image/COMP249/beforeLatex.png" alt="" style="width:100%">
-	 </div>
-  <div class="column">
-	<img src="./projectStyle/image/COMP249/ProcessingLatex.gif" alt="" style="width:100%">
-	 </div>
+	<div class="column">
+		<img src="./projectStyle/image/COMP249/beforeLatex.png" alt="" style="width:100%">
+	</div>
+	<div class="column">
+		<img src="./projectStyle/image/COMP249/ProcessingLatex.gif" alt="" style="width:100%">
+	</div>
 </div>
 <br />
-<h5 id ="ctr"> EXAMPLE OF INPUT FILE & SCREENSHOT DURING PROCESSING </h5>
+<h5 id="ctr"> EXAMPLE OF INPUT FILE & SCREENSHOT DURING PROCESSING </h5>
 
 <div class="row">
-   <div class="column">
-	 <img src="./projectStyle/image/COMP249/ACM.png" alt="" style="width:100%">
-	 </div>
-  <div class="column">
-	<img src="./projectStyle/image/COMP249/IEEE.png" alt="" style="width:100%">
-	 </div>
+	<div class="column">
+		<img src="./projectStyle/image/COMP249/ACM.png" alt="" style="width:100%">
+	</div>
+	<div class="column">
+		<img src="./projectStyle/image/COMP249/IEEE.png" alt="" style="width:100%">
+	</div>
 </div>
 
-<h5 id ="ctr"> DEMO EXAMPLE OF RESULT IN 2 CITATION FORMATS </h5>
+<h5 id="ctr"> DEMO EXAMPLE OF RESULT IN 2 CITATION FORMATS </h5>
 <ul>
-	<li id ="ctr"> <a href="https://www.acm.org/publications/authors/reference-formatting" target="_blank">ACM (Association for Computing Machinery)</a> </li>
-	<li id ="ctr"><a href="http://www.ijssst.info/info/IEEE-Citation-StyleGuide.pdf" target="_blank">IEEE</a></li>
+	<li id="ctr"> <a href="https://www.acm.org/publications/authors/reference-formatting" target="_blank">ACM (Association for Computing Machinery)</a> </li>
+	<li id="ctr"><a href="http://www.ijssst.info/info/IEEE-Citation-StyleGuide.pdf" target="_blank">IEEE</a></li>
 </ul>
 
 <!-- END OF REFERENCE GENERATOR -->
 
 <!-- PROJECT showcase: The art of command line -->
-<label id ="ctr"><h2>THE ART OF COMMAND LINE</h2></label>
-<label id ="ctr"><a href="https://github.com/DukeNgn/the-art-of-command-line" target="_blank">REPOSITORY</a></label>
+<label id="ctr">
+	<h2>THE ART OF COMMAND LINE</h2>
+</label>
+<label id="ctr"><a href="https://github.com/DukeNgn/the-art-of-command-line" target="_blank">REPOSITORY</a></label>
 <blockquote>
-Maintainer, contributor, and translator for an open-source project on GitHub, aims to provide developers with knowledge of command line control and applications on both Unix platforms and Windows in a precise document.
+	Maintainer, contributor, and translator for an open-source project on GitHub, aims to provide developers with knowledge of command line control and applications on both Unix platforms and Windows in a precise document.
 </blockquote>
 
-<!-- END OF PROJECT 2 SHOWCASE: COMP249 -->
+<!-- PROJECT showcase: POWERLINE-STYLE FOR TERMINAL -->
+<label id='ctr'>
+	<h2>CONFIGURE POWER-LINE STYLE FOR TERMINAL</h2>
+</label>
+<label id='ctr'><a href='./powerLineConfig.php'>DETAIL PAPER</a></label>
+<blockquote>
+	A detailed guideline on how to configure terminal shell to be <strong>powerline-style</strong> with <strong>ZSH Shell</strong> and <strong>Fish Shell</strong>.
+	<br />
+	Moreover, this guideline also introduces reader with knowledge of some popular shell package manager currently.
+	Therefore, reader can customize with their preferred settings, themes, or plugins.
+</blockquote>
+
 <br />
 
 <!-- SHOW CASE SIDE PROJECT PAPER -->
-<label id ="ctr"><h2>SIDE PROJECTS AND PAPERS</h2></label>
+<label id="ctr">
+	<h2>SIDE PROJECTS AND PAPERS</h2>
+</label>
 <!--showcase 1: MANA201 -->
 <h3><strong>MANA 201: Introduction to Business Management</strong></h3>
 <blockquote>A team project analysis the business model and the key for success of AT&T corporation </blockquote>
-<h4 id ="ctr"><a href="https://drive.google.com/open?id=1H8emQQJjwcpsMz_Zf2DotgF41EDqlh7l" target="_blank">[Proposal paper]</a></h4>
-<h4 id ="ctr"><a href="https://drive.google.com/open?id=1x_5h9jmiro9t3NSwBmPKjDtEQ2Owu3zG" target="_blank">[Final Essay]</a></h4>
+<h4 id="ctr"><a href="https://drive.google.com/open?id=1H8emQQJjwcpsMz_Zf2DotgF41EDqlh7l" target="_blank">[Proposal paper]</a></h4>
+<h4 id="ctr"><a href="https://drive.google.com/open?id=1x_5h9jmiro9t3NSwBmPKjDtEQ2Owu3zG" target="_blank">[Final Essay]</a></h4>
 <!------------------------>
 
 <?php include './share/footer.html' ?>
